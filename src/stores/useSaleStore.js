9ee48cdc9888dea5096b512263f1203df192a40b
pnzaou <perrinemmanuelnzaou@gmail.com>
2025-07-01 12:20:43 +0000

    ajout de la gestion du chargment et des données des clients provenants de la bd

--- src/stores/useSaleStore.js
+++ src/stores/useSaleStore.js
@@ -1,6 +1,29 @@
 import { create } from "zustand";
 
 export const useSaleStore = create((set) => ({
+    loading: false,
+
+    //gesttion des clients provenants de la bd pour les afficher dans le combobox client
+    clientsData: [],
+    getClientsData: async () => {
+        set({ loading: true });
+        try {
+            const res = await fetch("/api/client");
+            const body = await res.json();
+        
+            if (!res.ok) {
+              console.error("Impossible de récupérer les clients :", body.message);
+              return;
+            }
+        
+            set({ clientsData: body.data });
+        } catch (err) {
+            console.error("Erreur pendant la récupération des données :", err);
+        } finally {
+            set({ loading: false });
+        }
+    },
+
     //gestion des informations du client à envoyer lors de la soumission de la commande
     client: null,
     setClient: (client) => set({ client }),
