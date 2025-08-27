b252dd94ed6c25e7f046eb1b535cff828eb855b4
pnzaou <perrinemmanuelnzaou@gmail.com>
2025-08-27 13:37:03 +0000

    Ajout des constantes pour les modes de paiements

---
+++ src/constants/paymentMethods.js
@@ -0,0 +1,8 @@
+export const PAYMENT_METHODS = [
+  { value: "espèce", label: "Espèces" },
+  { value: "carte de crédit", label: "Carte de crédit" },
+  { value: "Wave", label: "Wave" },
+  { value: "Orange Money", label: "Orange Money" },
+  { value: "Free Money", label: "Free Money" },
+  { value: "account", label: "Compte client" }, // backend uses "account"
+];
